1b7b39d81717de580fcdb016799c0184fe1f266b
schiZis <stahanovgod@gmail.com>
2022-09-14 14:11:03 +0500

    fighting with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import s from './App.module.scss'
+import Input from "./components/Input/Input";
 
 function App() {
-  return (
-    <div className="App">
-      123
-    </div>
-  );
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const inputs = [
+        {
+        id: "username",
+        name: "UserName",
+        type: "text",
+        placeholder: "Username",
+        },
+        {
+            id: "email",
+            name: "Email",
+            type: "text",
+            placeholder: "@gmail.com",
+        },
+    ]
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({...values, [e.target.name]: e.target.value});
+    }
+
+    return (
+        <div className={s.app}>
+            <form action="">
+                {inputs.map((input) => (
+                    <Input
+                        key={input.id}
+                        id={input.id}
+                        name={input.name}
+                        placeholder={input.placeholder}
+                        type={input.type}
+                        value={values[input.name]}
+                        onChange={handleChange}
+                    />
+                ))}
+            </form>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,30 @@
+import React, {FC} from 'react';
+import s from './Input.module.scss';
+
+interface InputProps {
+    id: string,
+    name: string,
+    placeholder: string,
+    type: string,
+    value: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+const Input: FC<InputProps> = ({id, name, placeholder, type, value, onChange}) => {
+    return (
+        <div className={s.input}>
+            <label htmlFor={id}>
+                {name}
+            </label>
+            <input
+                id={id}
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+export default Input;
